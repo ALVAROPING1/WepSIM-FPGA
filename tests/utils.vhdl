7995b94884f4e88f19e434c4f07cdbc88f300991
ALVAROPING1 <alvaroping1@gmail.com>
2025-10-21 11:35:59 +0200

    feat(display_7segment): add 7 segment display controller

---
+++ tests/utils.vhdl
@@ -0,0 +1,22 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+package utils is
+    procedure clk_gen(
+        signal clk: out std_ulogic;
+        constant period: time := 2 us
+    );
+end package utils;
+
+package body utils is
+    procedure clk_gen(
+        signal clk: out std_ulogic;
+        constant period: time := 2 us
+    ) is
+    begin
+        loop
+            clk <= '1', '0' after period/2;
+            wait for period;
+        end loop;
+    end;
+end package body utils;
