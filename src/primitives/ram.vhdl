f703183cbf0330a9d32688c15ecbecee159c7998
ALVAROPING1 <alvaroping1@gmail.com>
2025-12-27 23:13:11 +0100

    fix(RAM): fix RAM not synthesizing as block RAM with non-0 initial value

--- src/primitives/ram.vhdl
+++ src/primitives/ram.vhdl
@@ -14,16 +14,16 @@ end;
 architecture behaviour of RAM is
     type State is array (natural range 0 to 2**addr_size - 1) of std_ulogic_vector(data'range);
     signal contents: State := (others => (others => '0'));
-    signal raddr: natural range State'range;
+    signal rdata: std_ulogic_vector(data'range);
 begin
     process(clk)
     begin
         if clk'event and clk = clk_edge then
-            raddr <= to_integer(addr);
+            rdata <= contents(to_integer(addr));
             if w then
                 contents(to_integer(addr)) <= data;
             end if;
         end if;
     end process;
-    data <= contents(raddr) when not w and r else (others => 'Z');
+    data <= rdata when not w and r else (others => 'Z');
 end;
