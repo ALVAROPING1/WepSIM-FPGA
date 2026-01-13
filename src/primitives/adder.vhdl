cac36a29cdc6cb3b9c29bb2c1c8f088539a932eb
ALVAROPING1 <alvaroping1@gmail.com>
2026-01-13 12:25:53 +0100

    feat(adder): allow to perform unsigned arithmetic

--- src/primitives/adder.vhdl
+++ src/primitives/adder.vhdl
@@ -8,17 +8,23 @@ entity Adder is
         a, b: in signed(size - 1 downto 0);
         c: out signed(size - 1 downto 0);
         subtraction: in std_ulogic;
-        overflow: out std_ulogic
+        signed_arith: in std_ulogic;
+        overflow, carry: out std_ulogic
     );
 end;
 
 architecture behaviour of Adder is
     signal a_extended, b_extended, c_extended: signed(size downto 0);
 begin
-    a_extended <= resize(a, size + 1);
-    b_extended <= resize(b, size + 1);
+    a_extended(a'range) <= a;
+    b_extended(b'range) <= b;
+    a_extended(size) <= a(a'high) when signed_arith else '0';
+    b_extended(size) <= b(b'high) when signed_arith else '0';
     c_extended <= a_extended + b_extended when not subtraction else a_extended - b_extended;
 
-    c <= c_extended(size - 1 downto 0);
+    c <= c_extended(c'range);
+    -- Carry should be ignored on signed arithmetic, and overflow should be
+    -- ignored on unsigned arithmetic
+    carry <= c_extended(size);
     overflow <= '1' when c_extended(size) /= c_extended(size - 1) else '0';
 end;
--- tests/primitives/adder_tb.vhdl
+++ tests/primitives/adder_tb.vhdl
@@ -9,35 +9,34 @@ end;
 
 architecture tb of Adder_TB is
     signal a, b, c: signed(size - 1 downto 0);
-    signal overflow, subtraction: std_ulogic;
+    signal overflow, carry, subtraction, signed_arith: std_ulogic;
 begin
-    dut: entity src.Adder generic map(size) port map(a, b, c, subtraction, overflow);
+    dut: entity src.Adder generic map(size) port map(a, b, c, subtraction, signed_arith, overflow, carry);
 
     main: process
         variable rnd: RandomPType;
-        variable res: integer;
+        variable res, int_a, int_b: integer;
         variable res_signed: signed(size downto 0);
         variable op: string(1 to 3);
+        variable signed_op: string(1 to 11);
     begin
         test_runner_setup(runner, runner_cfg);
         for iteration in 0 to 2000 loop
             a <= rnd.RandSigned(size);
             b <= rnd.RandSigned(size);
+            subtraction <= '1' when rnd.RandBool else '0';
+            signed_arith <= '1' when rnd.RandBool else '0';
             wait for 1 ns;
-            if run("addition") then
-                res := to_integer(a) + to_integer(b);
-                subtraction <= '0';
-                op := " + ";
-            elsif run("subtraction") then
-                res := to_integer(a) - to_integer(b);
-                subtraction <= '1';
-                op := " - ";
-            end if;
-            info("Operation: " & to_string(to_integer(a)) & op & to_string(to_integer(b)));
-            wait for 1 ns;
+            op := " - " when subtraction else " + ";
+            signed_op := " (signed)  " when signed_arith else " (unsigned)";
+            int_a := to_integer(a) when signed_arith else to_integer(unsigned(a));
+            int_b := to_integer(b) when signed_arith else to_integer(unsigned(b));
+            info("Operation: " & to_string(int_a) & op & to_string(int_b) & signed_op);
+            res := int_a - int_b when subtraction else int_a + int_b;
             check_equal(c, to_signed(res, size), "Check result");
             res_signed := to_signed(res, size + 1);
             check_equal(overflow, resize(res_signed, size) /= res_signed, "Check overflow");
+            check_equal(carry, res < 0 or res > 2**size - 1, "Check carry");
         end loop;
         test_runner_cleanup(runner);
     end process;
