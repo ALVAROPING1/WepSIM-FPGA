d0548535fbbbf20adf25927cf908f436e61c920c
ALVAROPING1 <alvaroping1@gmail.com>
2026-01-31 20:35:15 +0100

    feat(segment_display): allow overriding display val with debug data

--- src/devices/segment_display.vhdl
+++ src/devices/segment_display.vhdl
@@ -3,11 +3,11 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity SegmentDisplay is
-    generic(size: positive);
     port(
-        clk, iow, ior: in std_ulogic;
+        clk, iow, ior, run: in std_ulogic;
         addr: in unsigned(15 downto 0);
-        data: inout std_logic_vector(size - 1 downto 0);
+        data: inout std_logic_vector(31 downto 0);
+        debug_data: in std_ulogic_vector(31 downto 0);
         segment: out std_ulogic_vector(7 downto 0);
         enable: out std_ulogic_vector(7 downto 0)
     );
@@ -15,15 +15,18 @@ end;
 
 architecture behaviour of SegmentDisplay is
     signal display_hex: std_ulogic;
-    signal data_low, data_high: std_ulogic_vector(data'range) := (others => '0');
+    signal data_segments: std_ulogic_vector(63 downto 0) := (others => '0');
+    signal display_data: std_ulogic_vector(31 downto 0) := (others => '0');
     signal curr_segment: std_ulogic_vector(7 downto 0);
-    signal idx: natural range 0 to size - 1;
+    signal idx: natural range 0 to 7 - 1;
 begin
-    mode_reg: entity work.MemoryMappedReg generic map (1, x"5100")    port map (clk, iow, ior, addr, data(0) => data(0), data_out(0) => display_hex);
-    low_reg:  entity work.MemoryMappedReg generic map (size, x"5104") port map (clk, iow, ior, addr, data, data_low);
-    high_reg: entity work.MemoryMappedReg generic map (size, x"5108") port map (clk, iow, ior, addr, data, data_high);
+    mode_reg: entity work.MemoryMappedReg generic map (1,  x"5100") port map (clk, iow, ior, addr, data(0) => data(0), data_out(0) => display_hex);
+    low_reg:  entity work.MemoryMappedReg generic map (32, x"5104") port map (clk, iow, ior, addr, data, display_data);
+    high_reg: entity work.MemoryMappedReg generic map (32, x"5108") port map (clk, iow, ior, addr, data, data_segments(63 downto 32));
 
-    display:  entity work.SegmentDisplayController generic map (8) port map (acc(acc'high), display_hex, data_high & data_low, curr_segment, idx);
+    data_sel: entity work.Multiplexer generic map (32, 1) port map ((debug_data, display_data), data_segments(31 downto 0), sel(0) => run);
+
+    display:  entity work.SegmentDisplayController generic map (8) port map (clk, not run or display_hex, data_segments, curr_segment, idx);
 
     enable <= not ("00000001" sll idx);
     segment <= not curr_segment;
