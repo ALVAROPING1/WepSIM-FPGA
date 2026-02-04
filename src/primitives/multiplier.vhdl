8f84cb7a005213ab9dd1a1914ee7a11997cfa16a
ALVAROPING1 <alvaroping1@gmail.com>
2026-02-04 12:11:45 +0100

    feat(multiplier): allow unsigned and mixed sign multiplications

--- src/primitives/multiplier.vhdl
+++ src/primitives/multiplier.vhdl
@@ -5,7 +5,8 @@ use ieee.numeric_std.all;
 entity Multiplier is
     generic(size: positive);
     port(
-        a, b: in signed(size - 1 downto 0);
+        a, b: in std_ulogic_vector(size - 1 downto 0);
+        signed_a, signed_b: in std_ulogic;
         c: buffer signed(size * 2 - 1 downto 0);
         overflow: out std_ulogic
     );
@@ -13,8 +14,11 @@ end;
 
 architecture behaviour of Multiplier is
     signal sign_bit: signed(size downto 0);
+    signal res: signed(c'high + 2 downto 0);
+    use work.utils.maybe_signed;
 begin
-    c <= a * b;
+    res <= maybe_signed(a, signed_a) * maybe_signed(b, signed_b);
+    c <= res(c'range);
 
     sign_bit <= (others => c(c'high));
     overflow <= '1' when c(size * 2 - 1 downto size - 1) /= sign_bit else '0';
--- tests/primitives/multiplier_tb.vhdl
+++ tests/primitives/multiplier_tb.vhdl
@@ -1,5 +1,7 @@
 context work.tb_context;
 
+use src.utils.maybe_signed;
+
 entity Multiplier_TB is
     generic (
         runner_cfg: string;
@@ -8,25 +10,32 @@ entity Multiplier_TB is
 end;
 
 architecture tb of Multiplier_TB is
-    signal a, b: signed(size - 1 downto 0);
+    signal a, b: std_ulogic_vector(size - 1 downto 0);
     signal c: signed(size * 2 - 1 downto 0);
-    signal overflow: std_ulogic;
+    signal signed_a, signed_b, overflow: std_ulogic;
 begin
-    dut: entity src.Multiplier generic map(size) port map(a, b, c, overflow);
+    dut: entity src.Multiplier generic map(size) port map(a, b, signed_a, signed_b, c, overflow);
 
     main: process
         variable rnd: RandomPType;
         variable res: signed(size * 2 - 1 downto 0);
+        variable int_a, int_b: integer;
     begin
         test_runner_setup(runner, runner_cfg);
         for iteration in 0 to 2000 loop
-            a <= rnd.RandSigned(size);
-            b <= rnd.RandSigned(size);
-            wait for 1 ns;
-            res := to_signed(to_integer(a) * to_integer(b), size * 2);
-            info("Operation: " & to_string(to_integer(a)) & " * " & to_string(to_integer(b)));
-            check_equal(c, res, "Check result");
-            check_equal(overflow, resize(res, size) /= res, "Check overflow");
+            a <= rnd.RandSlv(size);
+            b <= rnd.RandSlv(size);
+            for signs in 0 to 3 loop
+                signed_a <= to_unsigned(signs, 2)(0);
+                signed_b <= to_unsigned(signs, 2)(1);
+                wait for 1 ns;
+                int_a := to_integer(maybe_signed(a, signed_a));
+                int_b := to_integer(maybe_signed(b, signed_b));
+                res := to_signed(int_a * int_b, size * 2);
+                info("Operation: " & to_string(int_a) & " * " & to_string(int_b));
+                check_equal(c, res, "Check result");
+                check_equal(overflow, resize(res, size) /= res, "Check overflow");
+            end loop;
         end loop;
         test_runner_cleanup(runner);
     end process;
