73cdec03b991f0231c4e849719e00ffe39c9c63a
ALVAROPING1 <alvaroping1@gmail.com>
2026-04-08 22:36:20 +0200

    chore: increase system clock speed

--- clk_divider.tcl
+++ clk_divider.tcl
@@ -3,13 +3,13 @@ create_ip -name clk_wiz -vendor xilinx.com -library ip -version 6.0 -module_name
 # Configure component
 # NOTE: values obtained from vivado's GUI wizard
 set_property -dict [list \
-  CONFIG.CLKOUT1_JITTER {333.843} \
-  CONFIG.CLKOUT1_PHASE_ERROR {293.793} \
-  CONFIG.CLKOUT1_REQUESTED_OUT_FREQ {15} \
+  CONFIG.CLKOUT1_JITTER {181.828} \
+  CONFIG.CLKOUT1_PHASE_ERROR {104.359} \
+  CONFIG.CLKOUT1_REQUESTED_OUT_FREQ {25} \
   CONFIG.CLK_OUT1_PORT {clk_out} \
-  CONFIG.MMCM_CLKFBOUT_MULT_F {49.875} \
-  CONFIG.MMCM_CLKOUT0_DIVIDE_F {66.500} \
-  CONFIG.MMCM_DIVCLK_DIVIDE {5} \
+  CONFIG.MMCM_CLKFBOUT_MULT_F {9.125} \
+  CONFIG.MMCM_CLKOUT0_DIVIDE_F {36.500} \
+  CONFIG.MMCM_DIVCLK_DIVIDE {1} \
   CONFIG.PRIMARY_PORT {clk_in} \
   CONFIG.USE_LOCKED {false} \
   CONFIG.USE_RESET {false} \
